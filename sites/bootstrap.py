9800974ec6185e76832d885a20a375e9e6a74ca5
ShuaiYang <yanghaa@sina.com>
2018-10-11 01:47:35 +0800

    fix bootstrap.py

--- sites/bootstrap.py
+++ sites/bootstrap.py
@@ -1,11 +1,52 @@
---2017-04-17 09:05:33--  https://bootstrap.pypa.io/bootstrap-buildout.py
-正在解析主机 bootstrap.pypa.io (bootstrap.pypa.io)... 151.101.72.175
-正在连接 bootstrap.pypa.io (bootstrap.pypa.io)|151.101.72.175|:443... 已连接。
-已发出 HTTP 请求，正在等待回应... 200 OK
-长度：7458 (7.3K) [text/x-python]
-正在保存至: “bootstrap-buildout.py”
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Bootstrap a buildout-based project
 
-     0K .......                                               100% 36.1M=0s
+Simply run this script in a directory containing a buildout.cfg.
+The script accepts buildout command-line options, so you can
+use the -c option to specify an alternate configuration file.
 
-2017-04-17 09:06:40 (36.1 MB/s) - 已保存 “bootstrap-buildout.py” [7458/7458])
+$Id$
+"""
 
+import os, shutil, sys, tempfile, urllib2
+
+tmpeggs = tempfile.mkdtemp()
+
+ez = {}
+exec urllib2.urlopen('http://peak.telecommunity.com/dist/ez_setup.py'
+                     ).read() in ez
+ez['use_setuptools'](to_dir=tmpeggs, download_delay=0)
+
+import pkg_resources
+
+cmd = 'from setuptools.command.easy_install import main; main()'
+if sys.platform == 'win32':
+    cmd = '"%s"' % cmd # work around spawn lamosity on windows
+
+ws = pkg_resources.working_set
+assert os.spawnle(
+    os.P_WAIT, sys.executable, sys.executable,
+    '-c', cmd, '-mqNxd', tmpeggs, 'zc.buildout',
+    dict(os.environ,
+         PYTHONPATH=
+         ws.find(pkg_resources.Requirement.parse('setuptools')).location
+         ),
+    ) == 0
+
+ws.add_entry(tmpeggs)
+ws.require('zc.buildout')
+import zc.buildout.buildout
+zc.buildout.buildout.main(sys.argv[1:] + ['bootstrap'])
+shutil.rmtree(tmpeggs)
